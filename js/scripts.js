993750659675db140252f517ab38e3a6d9d4de0e
cmdr33 <driveguy44@gmail.com>
2018-12-19 07:20:59 -0500

    Created a news feed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-online = Math.floor(Math.random() * (6000 - 1)) + 35;
-console.log(online)
-$("#online").click(function(){
-	$("#output").text(online)
+$(document).ready(function(){
+	online = Math.floor(Math.random() * (5000 - 1)) + 10;
+	console.log(online)
+	$("#onlinefriends").text("("+online+")")
+	$("#messageicon").hover(function(){
+		$("ul li ul li").show();
+	});
+
+	$(".slidedown").click(function(){
+		console.log("lol")
+		$(".hidecomments").toggle(1000)
+	});
+	$(".slidedown2").click(function(){
+		console.log("lol")
+		$(".hidecomments2").toggle(1000)
+	});
+
 });
