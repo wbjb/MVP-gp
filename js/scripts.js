61fcf765cfa0a7f2253b359a4aeb58980a49fd9f
Alex Ogola <ogollaokumu@gmail.com>
2018-12-19 10:25:17 +0300

    header and footer section

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
-document.getElementById('frmSearch').onsubmit = function() {
-        window.location = 'http://www.google.com/search?q=site:file:///home/moringa/Desktop/samples/index.html ' + document.getElementById('txtSearch').value;
-        return false;
-window.location = 'http://google.com/?q=site:mysite.com+'+escape(document.getElementById('searchbar');
+/// Bussiness logic
+var promoters = []
+
+function Promoter(promoterName, eventName, eventLocation) {
+  this.promoterName= promoterName;
+  this.eventName = eventName;
+  this.eventLocation = eventLocation;
+  promoters.push(this);
+}
+
+
+// UI logic
+$(document).ready(function() {
+  // Promoter event submit button
+  $("#eventSubmitForm").submit(function(event) {
+    event.preventDefault();
+    // debugger;
+    var promoter1Name = $("#promoter1Name").val();
+    var event1Name = $("#event1Name").val();
+    var event1Location = $("#event1Location").val();
+    var promoter1 = new Promoter(promoter1Name, event1Name, event1Location);
+    $("#promoterDisplay").text(promoters[0].promoterName + " is hosting " + promoters[1].eventName + " at " + promoters[2].eventLocation + ". Come one, come all!").show();
+    $(".eventTimeline").show();
+  });
+
+});
